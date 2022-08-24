218a76874a1dabd731b3f1c4878bc2c19ec586c6
DESKTOP-H94GOJT\Tobiasz <firma.tobit@gmail.com>
2022-08-24 10:23:12 +0200

    template

---
+++ resources/views/blocks/hero.blade.php
@@ -0,0 +1,40 @@
+{{--
+  Title: Hero
+  Description:  Hero
+  Category: formatting
+  Icon: admin-comments
+  Keywords: testimonial quote
+  Mode: preview
+  EnqueueStyle: styles/main.css
+
+--}}
+
+<div data-{{ $block['id'] }} class="{{ $block['classes'] }}">
+    <div class="hero_cols">
+        <div class="hero_cols_left">
+            <?php if( get_field('heading') ) : ?>
+                <h1>{{ get_field('heading') }}</h1>
+            <?php endif; ?>
+            <?php if( get_field('subheading') ) : ?>
+                <p>{{ get_field('subheading') }}</p>
+            <?php endif; ?>
+            <?php if( get_field('button_hero') ) : ?>
+                <a class="btn" alt="{{ get_field('button_hero')['alt'] }}" href="{{ get_field('button_hero')['url'] }}">{{ get_field('button_hero')['title'] }}</a>
+            <?php endif; ?>
+            
+        </div>
+        <div class="hero_cols_right">
+            <?php if( get_field('image_hero') ) : ?>
+                <img src="{{ get_field('image_hero') }}">
+            <?php endif; ?>
+            
+        </div>
+    </div>
+</div>
+
+<style type="text/css">
+  [data-{{$block['id']}}] {
+    background: {{ get_field('background_color') }};
+    color: {{ get_field('text_color') }};
+  }
+</style>
